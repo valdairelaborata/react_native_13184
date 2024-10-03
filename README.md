57b7e90dd85c2b6c19deefdf7cbcc1e3f72ee0bd
Valdair <valdairelaborata@gmail.com>
2024-10-02 21:09:53 -0300

    Ajustes

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ npx create-expo-app my-app --template blank
 npx expo install react-dom react-native-web @expo/metro-runtime
 
 npx expo start
+
+
+
+https://reactnative.dev/docs/components-and-apis
--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [tarefa, setTarefa] = useState('')
+  const [mensagem, setMensagem] = useState('Tela inicial')
+
+  const adicionar = () => {
+    setMensagem('Registro adicionado!!')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title} >Lista de atividades</Text>
+      <TextInput style={styles.input}
+
+        placeholder='Nome da tarefa'
+        value={tarefa}
+        onChangeText={text => setTarefa(text)}
+      />
+
+      <TouchableOpacity style={styles.btnAdd} onPress={adicionar}>
+        <Text style={styles.btnAddText} >Adicionar</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -13,8 +31,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5'
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20
+  },
+  input: {
+    backgroundColor: '#fff',
+    padding: 10,
+    fontSize: 16,
+    marginBottom: 10
+  },
+  btnAdd: {
+    backgroundColor: '#a9a9a9',
+    alignItems: 'center'
+
+  },
+  btnAddText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
 });
